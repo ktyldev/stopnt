c3426a39011348ff0728d9ee175e85ac85c2a088
Cat Flynn <me@ktyl.dev>
2020-07-11 03:21:28 +0100

    super basic-ass ship floaty ship controller

---
+++ Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipController : MonoBehaviour
+{
+    private const string AXIS_ACCELERATE = "Vertical";
+    private const string AXIS_STRAFE = "Horizontal";
+
+    [Header("Control")]
+    [Range(10_000, 100_000)]
+    [SerializeField] private float _acceleration;
+    [Range(10_000, 100_000)]
+    [SerializeField] private float _strafe;
+
+    [SerializeField] private Transform _com;
+    [SerializeField] private Transform _graphics;
+    [SerializeField] private float _tilt;
+
+    [Header("Hover")]
+    [SerializeField] private ConfigurableJoint _repulsor;
+    [SerializeField] private float _spring;
+    [SerializeField] private float _damper;
+
+    private Rigidbody _rb;
+    private float _accelerationInput;
+    private float _strafeInput;
+
+    private void Awake()
+    {
+        // use the rb that's actually on the floor
+        _rb = _repulsor.GetComponent<Rigidbody>();
+        _rb.centerOfMass = _com.localPosition;
+    }
+
+    private void Start()
+    {
+        float maxForce = _repulsor.yDrive.maximumForce;
+        _repulsor.yDrive = new JointDrive
+        {
+            positionSpring = _spring,
+            positionDamper = _damper,
+            maximumForce = maxForce
+        };
+    }
+
+    private void Update()
+    {
+        _accelerationInput = Input.GetAxis(AXIS_ACCELERATE);
+        _strafeInput = Input.GetAxis(AXIS_STRAFE);
+
+        var rot = transform.localRotation;
+        rot *= Quaternion.Euler(Vector3.forward * _strafeInput * -_tilt);
+        _graphics.localRotation = rot;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce(transform.forward * _accelerationInput * _acceleration, ForceMode.Force);
+        _rb.AddForce(transform.right * _strafeInput * _strafe, ForceMode.Force);
+    }
+}
