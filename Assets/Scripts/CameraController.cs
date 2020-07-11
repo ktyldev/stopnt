f5e991e9cb57bdd305408f022f1252dc6376626f
Cat Flynn <me@ktyl.dev>
2020-07-11 05:47:47 +0100

    super janky vehicle controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private ShipController _ship;
+    [Range(0,1)]
+    [SerializeField] private float _smoothing;
+
+    private Transform _target;
+    private Vector3 _offset;
+
+    private void Awake()
+    {
+        _target = _ship.transform;
+        _offset = transform.position - _target.position;
+    }
+
+    // fixed update to match position updates of vehicle
+    private void FixedUpdate()
+    {
+        var offset = Quaternion.Euler(Vector3.up * _target.rotation.eulerAngles.y) * _offset; 
+        var target = _target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, target, _smoothing);
+    }
+}
