cb8e0f4d607621f0568ca88e48dd5f1e4acf3a3d
cyndrdev <me@cyndr.uk>
2020-07-12 19:28:58 +0100

    made end screen show time

--- Assets/Scripts/Gameplay/GameStateEnd.cs
+++ Assets/Scripts/Gameplay/GameStateEnd.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using TMPro;
 
 public class GameStateEnd : GameStateBehaviour
 {
     [SerializeField] [InputAxis] string m_ContinueButton;
     [SerializeField] GameObject m_GameEndUI;
+    [SerializeField] TextMeshProUGUI m_HUDTime;
+    [SerializeField] TextMeshProUGUI m_FinalTime;
 
     public override void StartState()
     {
         m_GameEndUI.SetActive( true );
+        m_FinalTime.SetText( m_HUDTime.text );
     }
 
     public override bool UpdateState( float timeDelta )
